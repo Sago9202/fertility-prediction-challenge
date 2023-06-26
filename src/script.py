d6f98b5837a86b40d4b404e3ff79a34dcebe691d
Santiago Gómez-Echeverry <64211354+Sago9202@users.noreply.github.com>
2023-06-26 13:47:48 +0200

    Update script.py

--- src/script.py
+++ src/script.py
@@ -59,6 +59,7 @@ def predict_outcomes(df):
             'aantalhh2019','partner2019', 'sted2019', 'belbezig2019', 
             'nettoink2019', 'oplmet2019', 'herkomstgroep2019',
             'burgstat2019', 'leeftijd2019', 'woonvorm2019', 'oplmet2019', 'aantalki2019']
+    nomem_encr = df["nomem_encr"]
     df = df.loc[:, keepcols]
     
     # Load your trained model from the models directory
